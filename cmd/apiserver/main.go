0288b56ed82e0dc6b78b6a2f16c9f3620be9c28c
BincomAD <100017314+BincomAD@users.noreply.github.com>
2023-09-21 01:50:40 +0300

    PCF-26: Added default main function, config-file and folders structure

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "flag"
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+}
