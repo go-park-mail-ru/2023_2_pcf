299d9d9e1ab9c6345a041d058be18bc7d84ecb18
nikita <nikita.borisenckow@yandex.com>
2023-10-02 18:51:01 +0300

    works well. Code was separeted by files

--- internal/app/apiserver/authhandler.go
+++ internal/app/apiserver/authhandler.go
@@ -1,61 +1,15 @@
 package apiserver
 
 import (
+	"AdHub/internal/app/auth"
 	"AdHub/internal/app/models"
-	"crypto/rand"
-	"database/sql"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"net/http"
-	"sync"
+	"time"
 )
 
-type SessionHandler struct {
-	Sessions map[string]int //key - session token, value - user_id
-	mutex    sync.Mutex
-}
-
-func (sH *SessionHandler) AddSession(token string, userID int) {
-	sH.mutex.Lock()
-	defer sH.mutex.Unlock()
-
-	sH.Sessions[token] = userID
-}
-
-type Session struct {
-	token string `json:"token"`
-}
-
-var MySessionHandler = SessionHandler{
+var MySessionStorage = auth.SessionStorage{
 	Sessions: make(map[string]int),
-}
-
-func genToken(length int) (str string, err error) {
-	b := make([]byte, length)
-	_, err = rand.Read(b)
-	if err != nil {
-		return str, err
-	}
-	str = base64.URLEncoding.EncodeToString(b)
-	return str, err
-}
-
-func GetUserFromRows(rows *sql.Rows, user *models.User) error {
-	if rows.Next() {
-		if err := rows.Scan(&user.Id, &user.Login, &user.Password, &user.FName, &user.LName); err != nil {
-			return err
-		}
-	} else {
-		return errors.New("user not found")
-	}
-
-	err := rows.Err()
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (s *APIServer) AuthHandler(w http.ResponseWriter, r *http.Request) {
@@ -68,41 +22,45 @@ func (s *APIServer) AuthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	userSqlRows, err := s.Store.User().Get(user.Login)
+	userFromDB, err := s.Store.User().Read(user.Login)
 	if err != nil {
-		http.Error(w, "User check error: "+err.Error(), http.StatusBadRequest)
+		http.Error(w, "Error while getting user from DB: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	userToCheck := &models.User{}
-	err = GetUserFromRows(userSqlRows, userToCheck)
-	if err != nil {
-		//do smg...
-	}
-
-	if user.Password == userToCheck.Password {
-		sToken, err := genToken(32)
+	if user.Password == userFromDB.Password {
+		newSession := auth.Session{UserId: userFromDB.Id}
+		err = newSession.SetToken()
 		if err != nil {
 			//do smg...
 		}
-		for _, exists := MySessionHandler.Sessions[sToken]; exists; _, exists = MySessionHandler.Sessions[sToken] {
-			sToken, err = genToken(32)
+		//check if this token already exists
+		for contains := MySessionStorage.Contains(newSession.Token); contains; MySessionStorage.Contains(newSession.Token) {
+			err = newSession.SetToken()
 			if err != nil {
 				//do smg...
 			}
 		}
-		MySessionHandler.AddSession(sToken, userToCheck.Id)
+		MySessionStorage.AddSession(newSession)
 
-		newSession := Session{token: sToken}
 		w.WriteHeader(http.StatusCreated) // HTTP Status - 201
 		w.Header().Set("Content-Type", "application/json")
 		responseJSON, err := json.Marshal(newSession)
 		if err != nil {
+			defer MySessionStorage.RemoveSession(newSession.Token)
 			http.Error(w, "Failed to marshal JSON: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		cookie := &http.Cookie{
+			Name:     "session_token",
+			Value:    newSession.Token,
+			Expires:  time.Now().Add(10 * time.Hour),
+			HttpOnly: true,
+		}
+		http.SetCookie(w, cookie)
 		w.Write(responseJSON)
-		return
+
 	} else {
 		http.Error(w, "User check error: "+err.Error(), http.StatusBadRequest)
 		return
