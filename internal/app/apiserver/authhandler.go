bc4c5e2cd3a894bd90e369bea9a9fc01d95076d3
nikita <nikita.borisenckow@yandex.com>
2023-10-04 18:59:09 +0300

    done

--- internal/app/apiserver/authhandler.go
+++ internal/app/apiserver/authhandler.go
@@ -21,6 +21,7 @@ func (s *APIServer) AuthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:8081")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
+	//Парсинг юзера из запроса
 	var user models.User
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&user); err != nil {
@@ -29,35 +30,48 @@ func (s *APIServer) AuthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
+	//Валидация пришедших данных
+	if !(user.ValidateEmail() && user.ValidatePassword()) {
+		http.Error(w, "Invalid user params", http.StatusBadRequest)
+		return
+	}
+
+	//Получение реального юзера из бд по логину из запроса
 	userFromDB, err := s.Store.User().Read(user.Login)
 	if err != nil {
 		http.Error(w, "Error while getting user from DB: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	//Проверка пароля
 	if user.Password == userFromDB.Password {
 		newSession := auth.Session{UserId: userFromDB.Id}
 		err = newSession.SetToken()
 		if err != nil {
-			//do smg...
+			http.Error(w, "Error while token gen", http.StatusInternalServerError)
+			return
 		}
-		//check if this token already exists
+
+		//Проверка уникальности токена, перегенерация если он уже занят
 		for contains := auth.MySessionStorage.Contains(newSession.Token); contains; auth.MySessionStorage.Contains(newSession.Token) {
 			err = newSession.SetToken()
 			if err != nil {
-				//do smg...
+				http.Error(w, "Error while token gen", http.StatusInternalServerError)
+				return
 			}
 		}
 		auth.MySessionStorage.AddSession(newSession)
 
+		//Перевод структуры сессии в JSON
 		w.Header().Set("Content-Type", "application/json")
 		responseJSON, err := json.Marshal(newSession)
 		if err != nil {
 			defer auth.MySessionStorage.RemoveSession(newSession.Token)
-			http.Error(w, "Failed to marshal JSON: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Failed to marshal JSON:", http.StatusInternalServerError)
 			return
 		}
 
+		//Кукисет и возврат ответа (успех)
 		cookie := &http.Cookie{
 			Name:     "session_token",
 			Value:    newSession.Token,
