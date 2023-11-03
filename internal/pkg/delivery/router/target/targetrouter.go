e4fb78e0f664b9eececd38342e0fe6cecf9c97da
BincomAD <DeezPlay2003@gmail.com>
2023-11-03 23:40:26 +0300

    last correction

--- internal/pkg/delivery/router/target/targetrouter.go
+++ internal/pkg/delivery/router/target/targetrouter.go
@@ -26,14 +26,10 @@ func NewUserRouter(r *mux.Router, TargetUC entities.TargetUseCaseInterface, Sess
 }
 
 func ConfigureRouter(ur *TargetRouter) {
-<<<<<<< HEAD
-=======
-	//ur.router.HandleFunc("/ping", PingHandler).Methods("GET", "OPTIONS")
 	ur.router.HandleFunc("/targetcreate", ur.CreateTargetHandler).Methods("POST", "OPTIONS")
 	ur.router.HandleFunc("/targetedit", ur.UpdateTargetHandler).Methods("POST", "OPTIONS")
 	ur.router.HandleFunc("/targetdelete", ur.TargetDeleteHandler).Methods("DELETE", "OPTIONS")
 	ur.router.HandleFunc("/targetget", ur.GetTargetHandler).Methods("DELETE", "OPTIONS")
->>>>>>> origin/pcf-104-api-handlers
 
 	ur.router.Use(middleware.CORS)
 	ur.router.Use(middleware.Logger(ur.logger))
