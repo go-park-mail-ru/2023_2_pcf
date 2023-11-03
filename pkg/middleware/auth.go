d85b24b92dbf099c0f67b3a40bb4ffee64c42ecf
BincomAD <DeezPlay2003@gmail.com>
2023-11-04 00:30:26 +0300

    auth handler

---
+++ pkg/middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"AdHub/internal/pkg/entities"
+	"context"
+	"net/http"
+)
+
+func Auth(ss entities.SessionUseCaseInterface) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			sessionToken, err := r.Cookie("token")
+			if err != nil {
+				http.Error(w, "Not authorized", http.StatusUnauthorized)
+				return
+			}
+
+			userId, err := ss.GetUserId(sessionToken.Value)
+			if err != nil {
+				http.Error(w, "User not found", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), "userId", userId)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+
+}
