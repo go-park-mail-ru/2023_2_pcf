fd9c25dfbc8ee3841058c5fa9f55d6259b0f45b1
BincomAD <DeezPlay2003@gmail.com>
2023-11-16 19:49:57 +0300

    HW

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -7,24 +7,24 @@ CREATE TABLE "balance" (
 
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
-    login VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    f_name VARCHAR(255) DEFAULT NULL,
-    l_name VARCHAR(255) DEFAULT NULL,
-    s_name VARCHAR(255) DEFAULT "company",
-    avatar VARCHAR(255) DEFAULT "default.jpg"
+    login TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    f_name TEXT DEFAULT NULL,
+    l_name TEXT DEFAULT NULL,
+    s_name TEXT DEFAULT 'company',
+    avatar TEXT DEFAULT 'default.jpg',
     balance_id INT,
     FOREIGN KEY (balance_id) REFERENCES "balance" (id)
 );
 
 CREATE TABLE "ad" (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name TEXT NOT NULL,
     description TEXT DEFAULT NULL,
-    website_link VARCHAR(255) NOT NULL,
+    website_link TEXT NOT NULL,
     budget DECIMAL DEFAULT 0.0 NOT NULL,
     target_id INT,
-    image_link VARCHAR(255) NOT NULL,
+    image_link TEXT NOT NULL,
     owner_id INT,
     FOREIGN KEY (owner_id) REFERENCES "user" (id),
     FOREIGN KEY (target_id) REFERENCES "target" (id)
@@ -32,20 +32,13 @@ CREATE TABLE "ad" (
 
 CREATE TABLE "target" (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name TEXT NOT NULL,
     owner_id INT,
-    gender VARCHAR(10) DEFAULT NULL,
+    gender TEXT DEFAULT NULL,
     min_age INT DEFAULT 0, 
     max_age INT DEFAULT 127,
     tags TEXT DEFAULT NULL,
     keys TEXT DEFAULT NULL,
     regions TEXT DEFAULT NULL,
     interests TEXT DEFAULT NULL
-);
-
-CREATE TABLE "user_ads" (
-    user_id INT,
-    ad_id INT,
-    FOREIGN KEY (user_id) REFERENCES "user" (id),
-    FOREIGN KEY (ad_id) REFERENCES "ad" (id)
 );
